61bd4b09de8a5759c09ad68c712b66cbed53c20c
Bhagabata Parida <paridabapun@gmail.com>
2022-04-10 13:04:09 +0530

    Added

---
+++ src/com/hsbc/stringwrittenquestion/StringTest.java
@@ -0,0 +1,14 @@
+package com.hsbc.stringwrittenquestion;
+
+public class StringTest {
+
+	
+	public static void main(String[] args) {
+		
+		
+		String string = "AB";
+		String string1 = "Z";
+		System.out.println(string.compareTo(string1));
+		
+	}
+}
