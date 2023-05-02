eed6405abd64d0066d5faccb566b1450e8f8ad13
Bapun Parida <pariabapun@gmail.com>
2023-05-02 08:52:46 +0530

    Added

---
+++ src/com/hsbc/mcqs/EMCQ3.java
@@ -0,0 +1,19 @@
+package com.hsbc.mcqs;
+
+public class EMCQ3 {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 0;
+
+		try {
+			int c = a / b;
+			System.out.println(c);
+
+		} catch (Exception exception) {
+			System.out.println("Exception Caught");
+		/*} catch (ArithmeticException exception) {
+			System.out.println("Exception Caught");
+		}*/ // SHOULD be a unreachable block 
+	}
